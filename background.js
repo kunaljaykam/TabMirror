7a5a52001710270cd7ea41092f779b07d141bc00
Kunal Jaykam <kunaljaykam@pop-os.localdomain>
2023-11-29 19:24:58 +0530

    Improve the logics and msgs + fix icon

--- background.js
+++ background.js
@@ -2,25 +2,27 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.url) {
         chrome.tabs.query({}, (tabs) => {
             const duplicates = tabs.filter(t => t.url === tab.url && t.id !== tabId);
-            if (duplicates.length) {
-                console.log("Duplicate tab found: ", duplicates[0].id, tabId); // Debug log
-                chrome.tabs.sendMessage(tabId, { 
-                    type: "duplicate-tab-found", 
-                    duplicateTabId: duplicates[0].id 
+
+            duplicates.forEach(duplicateTab => {
+                let tabToClose = (duplicateTab.groupId === -1) ? duplicateTab.id : (tab.groupId === -1 ? tabId : (tabId < duplicateTab.id ? tabId : duplicateTab.id));
+                let isGrouped = tab.groupId !== -1 || duplicateTab.groupId !== -1;
+
+                chrome.tabs.sendMessage(tabId, {
+                    type: "confirm-tab-close",
+                    tabToClose: tabToClose,
+                    tabToFocus: tabId,
+                    isGrouped: isGrouped
                 });
-            }
+            });
         });
     }
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === "close-tab") {
-        console.log("Closing tab with ID:", message.tabId); // Debug log
-        chrome.tabs.remove(message.tabId, () => {
-            if (chrome.runtime.lastError) {
-                console.error("Error removing tab: ", chrome.runtime.lastError.message);
-            } else {
-                console.log("Tab closed successfully."); // Debug log
+        chrome.tabs.remove(message.tabToClose, () => {
+            if (!chrome.runtime.lastError) {
+                chrome.tabs.update(message.tabToFocus, { active: true });
             }
         });
     }
--- content.js
+++ content.js
@@ -1,11 +1,14 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "duplicate-tab-found") {
-        const userChoice = confirm("This tab is a duplicate. Would you like to close it?");
+    if (message.type === "confirm-tab-close") {
+        let messageText = message.isGrouped ? "Duplicate tab! Close and switch?" : "Close the other duplicate tab?";
+        const userChoice = confirm(messageText);
         if (userChoice) {
-            console.log("User chose to close the other tab:", message.duplicateTabId); // Debug log
-            chrome.runtime.sendMessage({ type: "close-tab", tabId: message.duplicateTabId });
-        } else {
-            console.log("User chose to keep the current tab:", sender.tab.id); // Debug log
+            chrome.runtime.sendMessage({ 
+                type: "close-tab", 
+                tabToClose: message.tabToClose,
+                tabToFocus: message.tabToFocus
+            });
         }
     }
 });
+
