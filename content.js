d3a1abd8528c044ab607dd3597dabb459c37d02f
Kunal Jaykam <kunaljaykam@pop-os.localdomain>
2023-11-30 16:35:46 +0530

    update msg

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === "confirm-tab-close") {
-        let messageText = message.isGrouped ? "Duplicate tab! Close and switch?" : "Close the other duplicate tab?";
+        let messageText = message.isGrouped ? "Duplicate tab! Close and switch?" : "Duplicate tab! Close the other tab?";
         const userChoice = confirm(messageText);
         if (userChoice) {
             chrome.runtime.sendMessage({ 
